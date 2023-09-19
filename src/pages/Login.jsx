ce7e082736499b736b257c0959404bd07f310a36
Brown Memoye <memoyebrown@gmail.com>
2023-09-19 11:19:38 +0100

    configure vercel routes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
     }
 
     return (
-        <div className="w-full h-screen">
+        <div className="w-full z-50  h-screen">
             <img className="hidden sm:block absolute w-full h-full object-cover" src={ signupbg } alt={ 'sign up to Movieez!' } />
             <div className="bg-black/60 fixed top-0 left-0 w-full h-screen">
 
