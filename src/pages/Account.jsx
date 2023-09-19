1f8dff09f0e6425c22e0a4ffccf307a4aabe7bcb
Brown Memoye <memoyebrown@gmail.com>
2023-09-19 10:51:41 +0100

    kkk

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,9 +5,7 @@ import { SavedItemsContextProvider, UserSaved } from '../context/SavedItemsConte
 
 const Account = () => {
     const { userName } = UserAuth()
-    const movies = UserSaved()?.movies
 
-    console.log('mm', movies)
     return (
         <>
             <div className='w-full text-white'>
@@ -19,11 +17,11 @@ const Account = () => {
                 <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]' />
                 <div className='absolute top-[20%] p-4 md:p-8'>
                     <h1 className='text-3xl md:text-5xl font-bold'><span className="capitalize">{ userName }</span>'s Favorites</h1>
-                    { !movies && <p className='italic text-gray-200 text-xs mt-5 md:text-5xl '>Your saved movies will appear below</p> }
+
                 </div>
             </div>
             <SavedItemsContextProvider>
-                { movies && <SavedMovies /> }
+                <SavedMovies />
 
             </SavedItemsContextProvider>
         </>
